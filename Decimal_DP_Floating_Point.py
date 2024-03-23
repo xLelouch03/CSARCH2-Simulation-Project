1d2378eab501dd049a6ee91240c4e93e4061a306
merepixel <89858634+merepixel@users.noreply.github.com>
2024-03-23 13:16:15 +0800

    Revert "NaN for nondigit or - inputs"
    This reverts commit f7563ad467da4a378a878f57a2c31c2de4b9cda5.

--- Decimal_DP_Floating_Point.py
+++ Decimal_DP_Floating_Point.py
@@ -367,7 +367,7 @@ def get_input_from_gui():
     
     if not all(char.isdigit() or char == '-' for char in decimal_input):
         # Set output fields for non-numeric input
-        display_output_in_gui("0", "", "", "11111", "00000000", ["00000000"]*5, "", "")
+        display_output_in_gui("0", "00000", "00000000", "11111", "00000000", ["00000000"]*5, "0000000000", "00000000000")
         return None, None, None
 
     if not validate_decimal_input(decimal_input):
@@ -393,16 +393,11 @@ def display_output_in_gui(sign_bit, decimal_input, exponent, combination_field, 
     output_text.insert(tk.END, f"Combination Field: {combination_field}\n")
     output_text.insert(tk.END, f"Exponent Extension: {exponent_extension}\n")
     output_text.insert(tk.END, "Coefficient Continuation: " + ' '.join(coefficient_continuation) + "\n")
-    
-    if decimal_input == "":
-      output_text.insert(tk.END, f"Binary Output: 0 11111 000000000 0000000000 0000000000 0000000000 0000000000 0000000000\n")
-      output_text.insert(tk.END, f"Hexadecimal Output: 7C00000000000000\n")
-    else:    
-      binary_output = sign_bit + combination_field + exponent_extension + ''.join(coefficient_continuation)
-      hexadecimal_output = convert_output_to_hexadecimal(binary_output)
-      binary_output_with_spaces = sign_bit + " " +  combination_field + " " + exponent_extension + " " + ''.join([' '.join(bits) for bits in [coefficient_continuation[i:i+10] for i in range(0, len(coefficient_continuation), 10)]])
-      output_text.insert(tk.END, f"Binary Output: {binary_output_with_spaces}\n")
-      output_text.insert(tk.END, f"Hexadecimal Output: {hexadecimal_output}\n")
+    binary_output = sign_bit + combination_field + exponent_extension + ''.join(coefficient_continuation)
+    hexadecimal_output = convert_output_to_hexadecimal(binary_output)
+    binary_output_with_spaces = sign_bit + " " +  combination_field + " " + exponent_extension + " " + ''.join([' '.join(bits) for bits in [coefficient_continuation[i:i+10] for i in range(0, len(coefficient_continuation), 10)]])
+    output_text.insert(tk.END, f"Binary Output: {binary_output_with_spaces}\n")
+    output_text.insert(tk.END, f"Hexadecimal Output: {hexadecimal_output}\n")
 
 # Function to convert and display output when Convert button is clicked
 def convert_and_display_output():
